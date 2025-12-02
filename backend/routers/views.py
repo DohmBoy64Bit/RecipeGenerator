cc7dd0a4a49b7b44bbeacb212ce70aff578cc1c0
NomadWithoutAHome <96033888+NomadWithoutAHome@users.noreply.github.com>
2025-12-02 00:19:22 -0500

    SEO

--- backend/routers/views.py
+++ backend/routers/views.py
@@ -9,3 +9,13 @@ router = APIRouter(tags=["views"])
 async def read_root():
     BASE_DIR = Path(__file__).resolve().parent.parent
     return FileResponse(BASE_DIR / "templates/index.html")
+
+@router.get("/sitemap.xml")
+async def get_sitemap():
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    return FileResponse(BASE_DIR / "static/sitemap.xml", media_type="application/xml")
+
+@router.get("/robots.txt")
+async def get_robots():
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    return FileResponse(BASE_DIR / "static/robots.txt", media_type="text/plain")
